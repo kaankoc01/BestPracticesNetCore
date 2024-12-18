2ae24d887206e685faf4381ac3f36c6fd34b1d7b
kaan koc <kaankoc01@gmail.com>
2024-12-18 18:44:03 +0300

    Veritabanı işlemleri -

---
+++ Repositories/Extension/RepositoryExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace App.Repositories.Extension
+{
+    public static class RepositoryExtensions
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services, IConfiguration configuration)
+        {
+            // veritabanı işlemleri
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                var connectionStrings = configuration.GetSection(ConnectionStringOption.Key).Get<ConnectionStringOption>();
+
+                options.UseSqlServer(connectionStrings!.SqlServer, sqlServerOptionsAction =>
+                {
+                    sqlServerOptionsAction.MigrationsAssembly(typeof(RepositoryAssembly).Assembly.FullName);
+                });
+
+            });
+            return services;
+
+        }
+    }
+}
